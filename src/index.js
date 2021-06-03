17553a9a526f56ec059e9d2e5bfad55818991b8a
king-handy <a.king.life@pm.me>
2021-06-03 19:56:17 -0400

    A

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ console.log('project is up')
 // 👉 TASK 1- Select the following elements from the DOM:
 
 // A- BUTTONS
-const launchButton = null
-const confirmButton = null
-const cancelButton = null
+const launchButton = document.getElementById('launchButton')
+const confirmButton = document.querySelector('#confirmButton')
+const cancelButton = document.querySelector('#cancelButton')
 
 // B- MESSAGES
-const successMessage = null
-const failureMessage = null
+const successMessage = document.querySelector('h1.report.success')
+const failureMessage = failureMessage = document.querySelector('h1.report.failure')
 
 // C- MODAL
 const modal = null
